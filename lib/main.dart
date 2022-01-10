ca03ebcdf85f1c3821f68f4cc6dd6bf993f41689
Lamine Diarra <id491112@gmail.com>
2022-01-10 15:26:40 +0000

    fin de l'interface d'accueil

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Menu(),
+      theme: ThemeData(brightness: Brightness.light),
+      home: const Menu(),
     );
   }
 }
