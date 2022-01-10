519bfde47b3bfbdae2a69b1bb9c5cf57286a2bfe
Lamine Diarra <id491112@gmail.com>
2022-01-10 12:25:32 +0000

    mis en place du menu

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_call_super
 
 import 'package:chat_app_1/models/page.dart';
+import 'package:chat_app_1/screens/setting.dart';
 import 'package:chat_app_1/services/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,15 +19,20 @@ class _MenuState extends State<Menu> {
   late PageController _controller;
   final List<MyPage> _pages = [
     MyPage(id: 0, icon: CupertinoIcons.chat_bubble_text, page: const Message()),
-    MyPage(id: 1, icon: CupertinoIcons.phone, page: const Message()),
+    MyPage(id: 1, icon: CupertinoIcons.phone, page: const Setting()),
     MyPage(id: 2, icon: CupertinoIcons.camera, page: const Message()),
-    MyPage(id: 3, icon: CupertinoIcons.settings, page: const Message())
+    MyPage(id: 3, icon: CupertinoIcons.settings, page: const Setting())
   ];
   @override
   void initState() {
     super.initState();
-    _controller = PageController();
-    _controller.addListener(() {});
+    _controller = PageController(
+      initialPage: 0,
+    );
+    _controller.addListener(() {
+      int page = _controller.page!.floor();
+      if (_currentPage != page) setState(() => _currentPage = page);
+    });
   }
 
   @override
@@ -37,31 +43,37 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: 70,
-        decoration: BoxDecoration(
-            border: Border(top: BorderSide(color: MyColors().navBorderColor))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: _pages
-              .map((page) => IconButton(
-                    enableFeedback: false,
-                    onPressed: () => setState(() => _currentPage = page.id),
-                    icon: Icon(
-                      page.icon,
-                      color: _currentPage == page.id
-                          ? MyColors().activeColor
-                          : MyColors().greyColor,
-                      size: 30,
-                    ),
-                  ))
-              .toList(),
+        bottomNavigationBar: Container(
+          height: 70,
+          decoration: BoxDecoration(
+              border:
+                  Border(top: BorderSide(color: MyColors().navBorderColor))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: _pages
+                .map((page) => IconButton(
+                      enableFeedback: false,
+                      onPressed: () {
+                        setState(() => _currentPage = page.id);
+                        _controller.animateToPage(_currentPage,
+                            duration: const Duration(milliseconds: 200),
+                            curve: Curves.ease);
+                      },
+                      icon: Icon(
+                        page.icon,
+                        color: _currentPage == page.id
+                            ? MyColors().activeColor
+                            : MyColors().greyColor,
+                        size: 30,
+                      ),
+                    ))
+                .toList(),
+          ),
         ),
-      ),
-      body: PageView(
-        controller: _controller,
-        children: _pages.map((page) => page.page).toList(),
-      ),
-    );
+        body: PageView.builder(
+          controller: _controller,
+          itemCount: _pages.length,
+          itemBuilder: (context, index) => _pages[index].page,
+        ));
   }
 }
